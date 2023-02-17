aec413783366d19a80e06e5367955d285876567a
sakibmahbub <arafatsakib48@gmail.com>
2023-02-17 12:30:23 +0600

    created calculator and clearing using event bubble

--- js/app.js
+++ js/app.js
@@ -18,3 +18,22 @@ document.getElementById("generate-pin").addEventListener("click", function () {
   const displayPin = document.getElementById("display-pin");
   displayPin.value = pin;
 });
+
+document.getElementById("calculator").addEventListener("click", function () {
+  const number = event.target.innerText;
+  const typedNumberField = document.getElementById("typed-number");
+  const previousTypedNumber = typedNumberField.value;
+  if (isNaN(number)) {
+    if (number === "C") {
+      typedNumberField.value = "";
+    } else if (number === "<") {
+      const digits = previousTypedNumber.split("");
+      digits.pop();
+      const remainingDigits = digits.join("");
+      typedNumberField.value = remainingDigits;
+    }
+  } else {
+    const newTypedNumber = previousTypedNumber + number;
+    typedNumberField.value = newTypedNumber;
+  }
+});
